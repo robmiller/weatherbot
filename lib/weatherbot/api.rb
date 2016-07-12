68ebd3ae1dd1faf159801326ec2d81442638c211
Rob Miller <rob@bigfish.co.uk>
2016-07-12 11:49:40 +0100

    Actually fetch data from the API

--- lib/weatherbot/api.rb
+++ lib/weatherbot/api.rb
@@ -21,7 +21,12 @@ module Weatherbot
     attr_reader :api_key
 
     def fetch_json(region, city)
-      File.read(Pathname(__dir__) + ".." + ".." + "sample.json")
+      uri = URI.parse("http://api.wunderground.com/api/#{api_key}/hourly/q/#{region}/#{city}.json")
+
+      response = Net::HTTP.get_response(uri)
+      fail "API returned a non-200 response" unless response.code == "200"
+
+      response.body
     rescue Object => e
       raise APIError, "Couldn't fetch the JSON from the API: #{e.message}"
     end
